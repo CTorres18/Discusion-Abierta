f74c83f89538c7a02ea2a112f9683d59de282a2f
Nico Delgado <nsdelgadov@gmail.com>
2016-09-07 03:01:47 -0300

    Diseño: Se mejora Completa propuesta quitando límite la texto de propuesta.

--- static/js/angular/app.js
+++ static/js/angular/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('DiscusionAbiertaApp', ['ngMaterial', 'LocalStorageModule', 'duScroll'])
+angular.module('DiscusionAbiertaApp', ['ngMaterial', 'LocalStorageModule', 'duScroll', 'monospaced.elastic'])
   .config(function ($httpProvider) {
     $httpProvider.defaults.xsrfCookieName = 'csrftoken';
     $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
