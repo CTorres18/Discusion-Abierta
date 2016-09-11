1b66200388470eefa6827db3e6fe6a4f6ae9e75c
Nico Delgado <nsdelgadov@gmail.com>
2016-09-11 03:11:31 -0300

    Diseño: Cambio en mensaje de correo

--- actas/EmailThreading.py
+++ actas/EmailThreading.py
@@ -13,7 +13,7 @@ class EmailThread(threading.Thread):
 
     def run(self):
         subject = "Participación en Discusión Abierta UChile"
-        message = "Estimad@: \n La ID de su propuesta es {0} \n Puede recuperar su acta en la pagina web https://discusionabierta.dcc.uchile.cl/actas/bajarpropuestadocx/{0} .\n “Este correo ha sido enviado automáticamente por el sistema de Discusión Abierta. Favor no responder. Para contactarse con el equipo tras esta plataforma comuníquese con Pablo Duarte:\npabduarte@uchile.cl\nSecretario Ejecutivo\nConsejo de Evaluación\nUniversidad de Chile\nFono: +56 229781148\nwww.uchile.cl/ConsejoEvaluacion.".format(
+        message = "Estimad@: \n La ID de su propuesta es {0} \n Puede recuperar su acta en la pagina web https://discusionabierta.dcc.uchile.cl/actas/bajarpropuestadocx/{0} .\n Este correo ha sido enviado automáticamente por el sistema de Discusión Abierta.\n Favor no responder.\n Para contactarse con el equipo tras esta plataforma comuníquese con Pablo Duarte:\npabduarte@uchile.cl\nSecretario Ejecutivo\nConsejo de Evaluación\nUniversidad de Chile\nFono: +56 229781148\nwww.uchile.cl/ConsejoEvaluacion.".format(
             self.ID)
         from_email = "propuestas@dcc.uchile.cl"
         recipient_list = []
