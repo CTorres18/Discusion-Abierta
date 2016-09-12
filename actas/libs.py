69eaee792d5dea043ec4b33363dcbd971811be80
nicolas <sparkscs@gmail.com>
2016-09-12 19:45:55 -0300

    Test email servidor #11

--- actas/libs.py
+++ actas/libs.py
@@ -632,6 +632,8 @@ def generar_propuesta_docx(acta):
 
 
 def generar_pre_propuesta_docx(acta):
+    string_acta = json.dumps(acta)
+    new_acta = json.loads(string_acta.encode('utf-8'))
     categorias = {2: u'Todos estamos de acuerdo',
                   1: u'La mayoría está de acuerdo',
                   0: u'No hay acuerdo de mayoría',
@@ -639,8 +641,9 @@ def generar_pre_propuesta_docx(acta):
                   -2: u'Todos estamos en desacuerdo'}
     tpl = DocxTemplate('static/templates_docs/pre_propuesta.docx')
     context = {}
-    context['acta'] = acta
+    context['acta'] = new_acta
     context['categorias'] = categorias
+
     tpl.render(context)
     f = StringIO()
     tpl.save(f)
