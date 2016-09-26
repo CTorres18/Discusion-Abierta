696e9c7f632ba56f955814ac167186545619640d
Nico Delgado <nsdelgadov@gmail.com>
2016-09-26 02:30:03 -0300

    parches para actualizar master

--- static/js/angular/controller_acta.js
+++ static/js/angular/controller_acta.js
@@ -1,5 +1,5 @@
 'use strict';
-var urlBase = "https://dabierta-dev.dcc.uchile.cl";
+var urlBase = "https://discusionabierta.dcc.uchile.cl";
 var LOCALSTORAGE_ACTA_KEY = 'acta';
 
 var app =angular.module('DiscusionAbiertaApp', ['ngMaterial', 'LocalStorageModule', 'monospaced.elastic'])
@@ -142,7 +142,7 @@ app.controller('ActaCtrl', function ($scope, $http, $mdDialog, localStorageServi
   $scope.showInfo = function(ev) {
     $mdDialog.show({
       controller: DialogController,
-      templateUrl: '/static/html/angular/get_actas_view_subir.html',
+      templateUrl: '/static/html/angular/not_get_actas_view.html',
       parent: angular.element(document.body),
       targetEvent: ev,
       clickOutsideToClose:true,
