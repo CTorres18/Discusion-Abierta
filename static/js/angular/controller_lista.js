c212c4f879da3b4b0c79ef611e70fc67bc0a1d60
Nico Delgado <nsdelgadov@gmail.com>
2016-10-10 03:49:11 -0300

    Lista: Se agrega Botón para descargar todo

--- static/js/angular/controller_lista.js
+++ static/js/angular/controller_lista.js
@@ -1,4 +1,6 @@
 'use strict';
+var urlBase = "localhost:8000";
+
 var app = angular.module('DiscusionAbiertaApp', ['ngMaterial', 'LocalStorageModule'])
 .config(function ($interpolateProvider) {
     $interpolateProvider.startSymbol('{$');
@@ -6,9 +8,11 @@ var app = angular.module('DiscusionAbiertaApp', ['ngMaterial', 'LocalStorageModu
   });
 
 app.controller('ListaCtrl', function ($scope, $http, $mdDialog, localStorageService,$mdToast, $location, $anchorScroll) {
+  $scope.datito=''
+  $scope.options_get = [{"name": "Origenes"},{"name": "Lugares"},{"name": "Encuentros"},{"name": "Estamentos"},{"name": "Respuestas"},{"name": "Temas"},{"name": "Participantes"},{"name": "Tipos_de_Encuentros"}]
+  
 
   var cargarPropuestas = function () {
-
     $http({
       method: 'GET',
       url: 'propuestas'
@@ -17,5 +21,53 @@ app.controller('ListaCtrl', function ($scope, $http, $mdDialog, localStorageServ
       console.log($scope.propuestas)
     });
   };
+
+  function DialogController($scope, $mdDialog) {
+    $scope.hide = function() {
+      $mdDialog.hide();
+    };
+
+    $scope.cancel = function() {
+      $mdDialog.cancel();
+    };
+
+    $scope.answer = function(answer) {
+      $mdDialog.hide(answer);
+    };
+  }
+
+  $scope.showInfo = function(ev) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: '/static/html/angular/get_actas_view_lista.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true,
+      fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+    })
+    .then(function(answer) {
+           if(answer.length >0) {
+             window.location.href = urlBase + '/actas/bajar/' + answer
+           }
+    }, function() {
+    });
+  };
+  $scope.getPropuesta = function(ev) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: '/static/html/angular/get_propuesta_view_lista.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true,
+      fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+    })
+    .then(function(answer) {
+            if(answer.length >0) {
+            window.location.href = urlBase + '/actas/bajarpropuestadocx/' + answer
+          }
+    }, function() {
+    });
+  };
+
   cargarPropuestas();
 });
