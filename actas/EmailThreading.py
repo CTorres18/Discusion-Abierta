f8448d66e9ed2b72c405e2d8805ec595869f7bb7
Nico Delgado <nsdelgadov@gmail.com>
2016-10-12 23:31:40 -0300

    Correo: Se envía correo diferenciado al CIRES

--- actas/EmailThreading.py
+++ actas/EmailThreading.py
@@ -4,7 +4,6 @@ from django.core.mail import send_mail, EmailMessage
 
 __author__ = 'Nicolas'
 
-
 class EmailThreadPropuesta(threading.Thread):
     def __init__(self, acta, ID):
         self.acta = acta
@@ -16,11 +15,26 @@ class EmailThreadPropuesta(threading.Thread):
         message = "Estimad@: \n Su Propuesta ha sido guardada en nuestro sistema, el código indentificador de su propuesta es {0} \n Puede recuperar su acta en la pagina web https://discusionabierta.dcc.uchile.cl/actas/bajarpropuestadocx/{0} .\n\n Este correo ha sido enviado automáticamente por el sistema de Discusión Abierta.\n Favor no responder.\n La Chile piensa la reforma: http://www.uchile.cl/discusion-reforma\n Dudas a +562 29780494 \n discusionreforma@uchile.cl\n Contacto equipo creador de la plataforma Discusión Abierta: \n contactodiscusionabierta@gmail.com".format(
             self.ID)
         from_email = "propuestas@dcc.uchile.cl"
-        recipient_list = ['discusionreforma@uchile.cl']
+        recipient_list = []
         for recipient in self.acta['participantes']:
             recipient_list.append(str(recipient['email']))
         encargado = self.acta['participante_organizador']
         recipient_list.append(str(encargado['email']))
+        mensaje_html = None
+        send_mail(subject, message, from_email, recipient_list, fail_silently=False, html_message=mensaje_html)
+
+class EmailThreadPropuestaCIRES(threading.Thread):
+    def __init__(self, acta, ID):
+        self.acta = acta
+        self.ID = ID
+        threading.Thread.__init__(self)
+
+    def run(self):
+        subject = "Participación en La Chile Piensa la Reforma"
+        message = "Estimad@: \n Se ha registrado una nueva propuesta cuyo identificador es {0} \n Puede descargar la propuesta en el siguiente link https://discusionabierta.dcc.uchile.cl/actas/bajarpropuestadocx/{0} .\n\n Este correo ha sido enviado automáticamente por el sistema de Discusión Abierta.\n Favor no responder.\n La Chile piensa la reforma: http://www.uchile.cl/discusion-reforma\n Dudas a +562 29780494 \n discusionreforma@uchile.cl\n Contacto equipo creador de la plataforma Discusión Abierta: \n contactodiscusionabierta@gmail.com".format(
+            self.ID)
+        from_email = "propuestas@dcc.uchile.cl"
+        recipient_list = ['discusionreforma@uchile.cl']
         mensaje_html = None
         send_mail(subject, message, from_email, recipient_list, fail_silently=False, html_message=mensaje_html)
 
@@ -42,6 +56,7 @@ class EmailThreadPrePropuesta(threading.Thread):
 
 def send_threaded_propuesta_mail(acta, ID):
     EmailThreadPropuesta(acta, ID).start()
+    EmailThreadPropuestaCIRES(acta, ID).start()
 
 
 def send_threaded_pre_propuesta_mail(acta, ID):
